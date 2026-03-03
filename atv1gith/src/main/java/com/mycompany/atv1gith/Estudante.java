9d51565386a9e383907ac1dfdcbca63c81ba4126
aluno <aluno@lab01-30>
2026-03-03 11:03:14 -0300

    nova class

--- atv1gith/src/main/java/com/mycompany/atv1gith/Estudante.java
+++ atv1gith/src/main/java/com/mycompany/atv1gith/Estudante.java
@@ -4,19 +4,32 @@
  */
 package com.mycompany.atv1gith;
 
-import java.util.Scanner;
-
+import java.util.Date;
 /**
  *
  * @author aluno
  */
 public class Estudante {
-    public static void main(String[] args) {
-        Scanner s = new Scanner (System.in);
+        
+        private String nome;
+        private Date dataNascimento;
+        private String cpf;
+        private char genero;
+        private String matricula;
+        private String email;
         
         
-        
-        
-    }
-    
+      public int obterIdade(Date hoje){
+         int idade = 0;
+         
+          //Lógica para calcular idade
+            return idade;
+           
+          
+  
+  }
+
+
+
+
 }
