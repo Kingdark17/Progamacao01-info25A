1e1485ad8e5aafa5fb1a70166088536806be28f2
aluno <aluno@lab02-20>
2026-03-09 14:18:37 -0300

    atualizaçao

---
+++ PedroHenrique/src/main/java/ifc/pedrohenrique/Estudante.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ifc.pedrohenrique;
+
+import java.util.Date;
+/**
+ *
+ * @author aluno
+ */
+public class Estudante {
+        
+        private String nome;
+        private Date dataNascimento;
+        private String cpf;
+        private char genero;
+        private String matricula;
+        private String email;
+        
+        public Estudante(){
+            this.nome = "Nobody";
+        }
+        
+      public int obterIdade(Date hoje){
+         int idade = 0;
+         
+          //Lógica para calcular idade
+            return idade;
+           
+  }
+      public String getNome (){
+          return this.nome;
+      }
+      
+      public void setNome(String _nome){
+          this.nome = _nome;
+      }
+
+
+
+
+}
