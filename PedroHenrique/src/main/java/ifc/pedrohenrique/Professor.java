750ca11aa25473891a97b3affb48046cae047a2c
aluno <aluno@lab01-30>
2026-03-17 11:01:02 -0300

    atualizacao

---
+++ PedroHenrique/src/main/java/ifc/pedrohenrique/Professor.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifc.pedrohenrique;
+
+import java.util.Date;
+
+/**
+ *
+ * @author aluno
+ */
+public class Professor {
+    
+        private static String nome;
+        private static String SIAPE;
+        private String email; 
+        private String telefone;
+        private String TitulacaoAcademica;
+        private String areaDeAtuacao;
+        private String Formacao;
+        private Date dataNascimento;
+        private String cpf;
+        private char genero;
+        
+       
+       
+        private String dataDeIngresso;
+        private String SemestreInicial;
+        private String SituancaoAcademica;
+}
