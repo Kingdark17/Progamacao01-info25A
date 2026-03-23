c775a44d2e3ef4b0003b02b72330e612ab38b5ef
suporte <suporte@191.52.13.150>
2026-03-23 13:30:11 -0300

    atualizaçao 2.0

--- PedroHenrique/src/main/java/ifc/pedrohenrique/Estudante.java
+++ PedroHenrique/src/main/java/ifc/pedrohenrique/Estudante.java
@@ -1,51 +1,51 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ifc.pedrohenrique;
 
 import java.util.Date;
+
 /**
  *
  * @author aluno
  */
 public class Estudante {
-        
-        private static String nome;
-        private Date dataNascimento;
-        private String cpf;
-        private char genero;
-        private String matricula;
-        private String email;
-        private String nivelEnsino;
-        private String telefone;
-        private String dataDeIngresso;
-        private String SemestreInicial;
-        private String SituancaoAcademica;
-        private Sting digito;
-        
 
-        
-        public Estudante(){
-            this.nome = "Nobody";
-        }
-        
-      public int obterIdade(Date hoje){
-         int idade = 0;
-         
-          //Lógica para calcular idade
-            return idade;
-           
-  }
-      public static String getNome (){
-     
-          return nome;
-      }
-      
-      public static void setNome(String _nome){
-          nome = _nome;
-      }
-      public static void setMatricula(String digito){ 
-          digito = digito;
-      }
+    private static String nome;
+    private Date dataNascimento;
+    private String cpf;
+    private char genero;
+    private String matricula;
+    private String email;
+    private String nivelEnsino;
+    private String telefone;
+    private String dataDeIngresso;
+    private String SemestreInicial;
+    private String SituancaoAcademica;
+    private String digito;
+
+    public Estudante() {
+        this.nome = "Nobody";
+    }
+
+    public int obterIdade(Date hoje) {
+        int idade = 0;
+
+        //Lógica para calcular idade
+        return idade;
+
+    }
+
+    public static String getNome() {
+
+        return nome;
+    }
+
+    public static void setNome(String _nome) {
+        nome = _nome;
+    }
+
+    public static void setMatricula(String digito) {
+        digito = digito;
+    }
 }
