5d99a50bc60a1197573271197248e297df28ef42
aluno <aluno@lab01-01>
2026-04-07 10:27:06 -0300

    atualizaçao

---
+++ PedroHenrique/src/main/java/ifc/pedrohenrique/Diciplinas.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ifc.pedrohenrique;
+
+/**
+ *
+ * @author aluno
+ */
+public class Diciplinas {
+    private String nomeDiciplina;
+    private String codigo;
+    private String cargaHoraria;
+    private String ementa;
+    private String objetivoDaaprendizagem;
+        
+public Diciplinas(){
+    this.nomeDiciplina = "nobody";
+}
+public void nomeDiciplina(){
+
+}
+public void codigo(){
+
+}
+public void cargaHoraria(){
+
+}
+public void ementa(){
+
+}
+public void objetivo(){
+
+}
+
+
+    }
+
